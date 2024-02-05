2c450a7e691a2f9d1803400c866c8ac88698ba97
arithama <lucassoaresetp@gmail.com>
2024-02-05 16:34:13 -0300

    Added option to edit the profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, BooleanField, SelectField,\
     SubmitField
-from wtforms.validators import Required, Length, Email, Regexp
-from wtforms import ValidationError
-from ..models import Role, User
+from wtforms.validators import Length
 
 class EditProfileForm(FlaskForm):
     name = StringField('Real name', validators=[Length(0, 64)])
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
+from flask import render_template, redirect, url_for, flash
+from flask_login import login_required, current_user
 from . import main
-from flask import render_template, flash
-from ..models import User
+from .forms import EditProfileForm
 from .. import db
+from ..models import User
 
 
 @main.route('/')
@@ -18,5 +20,20 @@ def users():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        db.session.commit()
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
 
 
